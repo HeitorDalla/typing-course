2fde51fb15d420426480c7b40951d49b8d6b08a4
HeitorDalla <88400131+HeitorDalla@users.noreply.github.com>
2025-03-03 23:26:15 -0300

    Parte de validação ja foi feita, faltando a parte de digitação

--- script.js
+++ script.js
@@ -8,9 +8,10 @@ const niveis = [
     'A digitação não só aprimora a produtividade, mas também influencia a eficiência em ambientes de trabalho de alto desempenho, onde a comunicação rápida e precisa é imprescindível.'
 ];
 
-console.log(niveis[0])
-
 const links = document.querySelectorAll("#niveis li a");
+
+let intervaloTempo; // Variável para armazenar a váriavel tempo
+let tempoIniciado = false; // Para ver se o tempo já foi iniciado
 
 function atualizarTexto (indice) {
     const textoMudar = document.querySelector("#texto");
@@ -19,14 +20,56 @@ function atualizarTexto (indice) {
 
 links.forEach((link, index) => {
     link.addEventListener("click", (evento) => {
+        evento.preventDefault();
+        // Desabilitar o input
         const input = document.querySelector("#entrada");
-
         input.style.display = 'block'; // Torna o campo input visível
-
         input.disabled = false; // Habilita o campo input
 
-        evento.preventDefault();
         atualizarTexto(index);
     });
 });
 
+// Função para iniciar o tempo
+function iniciarContagemTempo () {
+    if (tempoIniciado) {
+        return; // Impede que a contagem começe mais de uma vez
+    }
+    tempoIniciado = true; // Marca que o tempo foi iniciado
+
+    const contadorTempo = document.querySelector("#contadorTempo");
+    let contadorTempoValue = parseInt(contadorTempo.innerHTML);
+
+    intervaloTempo = setInterval (() => {
+        if (contadorTempoValue === 0) { // Se chegou realmente à zero
+            clearInterval(intervaloTempo);
+
+            // Cria o elemento
+            const containerConteudo = contadorTempo.closest("#conteudo");
+            let errorSpan = criarElementos(containerConteudo);
+            errorSpan.innerHTML = `O seu tempo acabou!`;
+
+            const input = document.querySelector("#entrada");
+            input.disabled = true;
+
+        } else {
+            contadorTempoValue --; // Decrementa 1 segundo
+            contadorTempo.textContent = contadorTempoValue;
+        }
+    }, 1000);
+};
+
+// Função que cria elementos
+function criarElementos (father) {   
+    const novoElemento = document.createElement("span");
+    novoElemento.setAttribute("class", "error");
+    novoElemento.innerHTML = '';
+    father.appendChild(novoElemento);
+    return novoElemento;
+};
+
+// Evento que aciona a contagem do tempo assim que clica no input
+const entradaInput = document.querySelector("#entrada");
+entradaInput.addEventListener("click", (evento) => {
+    iniciarContagemTempo();
+});
